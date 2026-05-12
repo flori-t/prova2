c9b624590f0f27e35647807b9372b04680dcd36d
Aula - Firenze <floritrifi22@gmail.com>
2026-05-12 16:55:44 +0200

    Hangar Test metodo adddrone

--- hangar-drones-prova2/hangar-drones/tests/HangarTest.php
+++ hangar-drones-prova2/hangar-drones/tests/HangarTest.php
@@ -24,4 +24,19 @@ final class HangarTest extends TestCase
         self::assertSame(true, $hangar->hasFreeSlot());
     }
 
+    public function testAddDrone()
+    {
+        $hangar = new Hangar(4);
+
+        $drone1 = new Drone("JB1", 2, Drone::STATUS_DOCKED);
+        $drone2 = new Drone("JB2", 22, Drone::STATUS_MAINTENANCE);
+
+        $hangar->addDrone($drone1);
+        $hangar->addDrone($drone2);
+
+        self::assertSame(4, $hangar->capacity());
+        self::assertSame(1, $hangar->dockedCount());
+        self::assertSame(1, $hangar->maintenanceCount());
+    }
+
 }
